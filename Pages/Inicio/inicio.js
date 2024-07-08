6629dcb2ba8703fb7bb5d2592d1cca2ead63ba60
Lucas Teixeira <lucasgayosot@gmail.com>
2024-07-08 12:27:42 -0300

    add splash screen

--- Pages/Inicio/inicio.js
+++ Pages/Inicio/inicio.js
@@ -23,3 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
     showSlide(currentSlide);
     setInterval(nextSlide, 3000); // Change slide every 3 seconds
 });
+
+//splash screen
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById('splash-screen').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    }, 3000); // Splash screen aparecerá por 3 segundos
+});
